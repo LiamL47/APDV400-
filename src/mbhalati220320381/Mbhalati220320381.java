feccf456b91348d9c48b46f55d95ac8e9605b499
Lunghi Liam Mbhalati <220320381@ump.ac.za>
2021-05-14 14:18:07 +0200

    Initial commit.

---
+++ src/mbhalati220320381/Mbhalati220320381.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mbhalati220320381;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Liam
+ */
+public class Mbhalati220320381 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+       
+        
+       int firstNumber,secondNumber;
+       int sum;
+       
+       firstNumber= 25;
+       secondNumber = 15;
+               
+        sum = firstNumber + secondNumber;
+
+      System.out.println("Sum of these numbers: "+ sum);
+   }
+}
